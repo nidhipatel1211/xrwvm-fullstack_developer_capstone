7d88586bacbdb8f4c2f38969d18fb9b3969b7b0d
Nidhi Patel <nidhi121125@gmail.com>
2025-05-06 16:21:40 -0400

    Registration

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<RegistrationPanel />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
